4b1db9275d4a5324bf493ef148546d040fe7fa15
jhossie2002-cmyk <jhossie2002@gmail.com>
2026-02-04 15:59:02 +0000

    Create sitso_script7.R

---
+++ sitso/sitso_script7.R
@@ -0,0 +1,42 @@
+##EXERCISE 4.2
+
+#LOADING THE TIDYVERSE
+
+library(tidyverse)
+
+##QUESTION 1
+
+#LOADING THE BUILT IN DATA SET CO2
+data(co2)
+
+#ANSWER;co2 is tidy data: it has one year for each row
+
+##QUESTION 2
+
+#LOADING THE BUILT IN DATA SET CHICKWEIGHT
+data("ChickWeight")
+
+#ANSWER;ChickWeight is tidy: it is stored in a data frame
+
+##QUESTION 3
+
+#LOADING THE BUILT IN DATA SET BOD
+data(BOD)
+
+#ANSWER;BOD is tidy: each row is an observation with two values (time and demand)
+
+##QUESTION 4
+
+#LOADING THE FOLLOWING DATA SETS
+
+data(BJsales) #NOT TIDY
+
+data(EuStockMarkets)#TIDY
+
+data(DNase)#TIDY
+
+data(Formaldehyde)#TIDY
+
+data(Orange)#TIDY
+
+data(UCBAdmissions)#NOT TIDY
