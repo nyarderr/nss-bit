b97ae29a9469511883901ea84703f716b19d73f4
Naki Tetteh <nakitetteh23@gmail.com>
2026-02-16 21:53:13 +0000

    Create martha_script 6.R

---
+++ martha/martha_script 6.R
@@ -0,0 +1,22 @@
+##practicing
+library(tidyverse)
+##Exercise
+##Question one
+data(co2)
+# co2istidydata: ithasoneyearforeachrow
+
+##Question two
+data(ChickWeight)
+# ChickWeight is tidy: it is stored in a data frame.
+
+##Question three
+data(BOD)
+# BOD is tidy: each row is an observation with two values (time and demand
+
+##Question four
+data(BJsales) Not tidy
+data(EuStockMarkets) tidy
+data(DNase) tidy
+data(Formaldehyde) tidy
+data(Orange) tidy
+data(UCBAdmissions) not tidy
